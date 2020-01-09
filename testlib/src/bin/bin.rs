646621aae60a4d443c62ccf937db1f90494ace1c
Tyler Ruckinger <t.ruckinger@gmail.com>
2020-01-09 12:01:41 -0500

    Initial commit

---
+++ testlib/src/bin/bin.rs
@@ -0,0 +1,4 @@
+// This binary does not forbid unsafe code.
+// However that should not affec the results of cargo-geiger on this lib,
+// particularly if it is used as a dependency in another project.
+fn main() {}
